f1464d89a60c37622404ef7776f9e215f0b4d4c5
Ondřej Krejčíř <ondrej.krejcir@antee.cz>
2013-09-28 14:21:53 +0200

    read stdin & write stdout

--- src/pal/Main.java
+++ src/pal/Main.java
@@ -1,7 +1,17 @@
 package pal;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
 
+            }
+        } catch(IOException e) {
+        }
+        System.out.println("OUTPUT");
     }
 }
