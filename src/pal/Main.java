dee841ddfa827140bd26018ac930f884b1b18363
Ondřej Krejčíř <ondrej.krejcir@antee.cz>
2013-11-05 12:33:21 +0100

    determinant

--- src/pal/Main.java
+++ src/pal/Main.java
@@ -2,101 +2,87 @@ package pal;
 
 import java.io.*;
 import java.util.HashMap;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static HashMap<Integer,Stack<Integer>> edges = new HashMap<Integer, Stack<Integer>>();
-    public static Stack<Integer> stack = new Stack<Integer>();
-    public static int[] mcbp;
-    public static int crossings = -1;
+    public static HashMap<Integer, double[]> edges = new HashMap<Integer, double[]>();
+    public static int vertexCount = -1;
     public static int edgeCount = 0;
 
     public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("file.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line;
         StringTokenizer st;
         if ((line = br.readLine()) != null) {
             st = new StringTokenizer(line);
-            crossings = Integer.parseInt(st.nextToken());
+            vertexCount = Integer.parseInt(st.nextToken());
         }
-        if (crossings == 1) {
-            PrintWriter print = new PrintWriter(new OutputStreamWriter(System.out, "UTF-8"), false);
-            print.write(crossings + "\n0\n0 0\n");
-            print.flush();
-            System.exit(0);
-        }
-        mcbp = new int[crossings];
-        int start;
-        int end;
+        int start, end;
+        double[] edge;
         while (!(line = br.readLine()).equals("0 0")) {
             st = new StringTokenizer(line);
+            edge = new double[vertexCount - 1];
             start = Integer.parseInt(st.nextToken());
             end = Integer.parseInt(st.nextToken());
-            if (!edges.containsKey(start)) {
-                edges.put(start, new Stack<Integer>());
+            if (start < edge.length) {
+                edge[start] = 1;
             }
-            edges.get(start).add(end);
-            mcbp[start]--;
-            mcbp[end]++;
-            edgeCount++;
-        }
-        int startFrom = -1;
-        int endTo = -1;
-        for (int i = 0; i < crossings; i++) {
-            if (mcbp[i] != 0) {
-                if (mcbp[i] == -1 && startFrom == -1) {
-                    startFrom = i;
-                    continue;
-                }
-                if (mcbp[i] == 1 && endTo == -1) {
-                    endTo = i;
-                    continue;
-                }
-                System.out.println(-1);
-                System.exit(0);
+            if (end < edge.length) {
+                edge[end] = -1;
             }
+            edges.put(edgeCount++, edge);
         }
-        if (startFrom == -1) {
-            startFrom = 0;
-        }
-        stack = new Stack<Integer>();
-        euler(startFrom);
-        stack.push(startFrom);
-        print();
+        calc();
     }
 
-    public static void print() throws IOException {
-        PrintWriter print = new PrintWriter(new OutputStreamWriter(System.out, "UTF-8"), false);
-        print.write(crossings + "\n");
-        int round = 0;
-        int start = 0;
-        if (!stack.empty()) {
-            start = stack.pop();
-        }
-        while(!stack.empty()) {
-            round++;
-            print.write(start + " " + stack.peek() + "\n");
-            start = stack.pop();
-            if (round % 5000 == 0) {
-                print.flush();
+    public static void calc() {
+        int sum = 0;
+        char[] ary;
+        double[][] m = new double[vertexCount - 1][];
+        int counter;
+        int from = 0;
+        int to = (int) Math.pow(2, edgeCount);
+        for (int i = from; i < to; i++) {
+            if (Integer.bitCount(i) == vertexCount - 1) {
+                counter = 0;
+                ary = Integer.toBinaryString(i).toCharArray();
+                for (int j = 0; j < ary.length; j++) {
+                    if (ary[j] == '1') {
+                        m[counter++] = edges.get(((edgeCount) - ary.length) + j);
+                    }
+                }
+                if (isTree(m)) {
+                    sum++;
+                }
             }
         }
-        if (round == 0) {
-            print.write(start + "\n");
-        }
-        print.write("0 0\n");
-        print.flush();
+        System.out.println(sum);
     }
 
-    public static void euler(int start) {
-        Stack<Integer> s = edges.get(start);
-        while (s != null && !s.empty()) {
-            int end = s.pop();
-            euler(end);
-            stack.push(end);
-        }
+    public static boolean isTree(double[][] mat) {
+        return ((int)Math.round(determinant(mat))) != 0;
+    }
 
+    public static double determinant(double[][] mat) {
+        double result = 0;
+        if(mat.length == 1) {
+            result = mat[0][0];
+            return result;
+        }
+        if(mat.length == 2) {
+            result = mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0];
+            return result;
+        }
+        for(int i = 0; i < mat[0].length; i++) {
+            double temp[][] = new double[mat.length - 1][mat[0].length - 1];
+            for(int j = 1; j < mat.length; j++) {
+                System.arraycopy(mat[j], 0, temp[j-1], 0, i);
+                System.arraycopy(mat[j], i+1, temp[j-1], i, mat[0].length-i-1);
+            }
+            result += mat[0][i] * Math.pow(-1, i) * determinant(temp);
+        }
+        return result;
     }
 }
