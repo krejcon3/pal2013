225352c238f6584470a99f143eeed0f54f1813d9
Ondřej Krejčíř <ondrej.krejcir@antee.cz>
2013-11-21 23:05:34 +0100

    WIP

--- src/pal/Main.java
+++ src/pal/Main.java
@@ -1,15 +1,36 @@
 package pal;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static int[] NMQ = new int[3];
+    public static int[] BCTUVW = new int[6];
+
+
+
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("file.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line;
-            while ((line = br.readLine()) != null) {
-
+        StringTokenizer st;
+        int charCounter = 0;
+        if ((line = br.readLine()) != null) {
+            st = new StringTokenizer(line);
+            while (st.hasMoreTokens()) {
+                NMQ[charCounter++] = Integer.parseInt(st.nextToken());
             }
+        }
+        int lineCounter = 0;
+        while ((line = br.readLine()) != null) {
+            charCounter = 0;
+            st = new StringTokenizer(line);
+            if (lineCounter < NMQ[0]) {
+                while (st.hasMoreTokens()) {
+                    NMQ[charCounter++] = Integer.parseInt(st.nextToken());
+                }
+            }
+        }
         System.out.println("OUTPUT");
     }
 }
